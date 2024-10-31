5f3d8ceba93c6d134c6858ae110cec858b88da96
Jasper Lai <capsa2@gld.gov.hk>
2024-10-31 18:18:04 +0800

    in progress

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    './docs/**/*.html'
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'green':'hsl(75, 94%, 57%)',
+        'grey-700':'hsl(0, 0%, 20%)',
+        'grey-800':'hsl(0, 0%, 12%)',
+        'grey-900':'hsl(0, 0%, 8%)'
+      },
+      screens:{
+        'mobile':'375px',
+        'tablet':'768px',
+        'desktop':'1440px',
+      },
+      fontFamily:{
+        'inter':['Inter','sans-serif']
+      }
+    },
   },
   plugins: [],
 }
